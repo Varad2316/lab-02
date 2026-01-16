3aefb30939b277ac549e09bf7cddeaac840f8237
Varad P <varad@ualberta.ca>
2026-01-16 15:00:31 -0700

    Implemented ListyCity

---
+++ app/src/main/java/com/example/listycity/MainActivity.java
@@ -0,0 +1,96 @@
+package com.example.listycity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+ListView cityList;
+ArrayAdapter cityAdapter;
+ArrayList<String> dataList;
+private Button addButton;
+private Button deleteButton;
+private Button confirmButton;
+private EditText editCityName;
+private LinearLayout inputLayout;
+private int selectedPosition = -1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        cityList = findViewById(R.id.city_list);
+        addButton = findViewById(R.id.button_add);
+        deleteButton = findViewById(R.id.button_delete);
+        confirmButton = findViewById(R.id.button_confirm);
+        editCityName = findViewById(R.id.edit_city_name);
+        inputLayout = findViewById(R.id.input_layout);
+
+        String [] cities = {"Edmonton","Calgary","Toronto","Winnipeg"};
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this,R.layout.content,dataList);
+        cityList.setAdapter(cityAdapter);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputLayout.setVisibility(View.VISIBLE);
+                editCityName.setText("");
+            }
+        });
+
+        confirmButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String cityName=editCityName.getText().toString();
+                dataList.add(cityName);
+                cityAdapter.notifyDataSetChanged();
+                inputLayout.setVisibility(View.GONE);
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (selectedPosition!=-1) {
+                    dataList.remove(selectedPosition);
+                    cityAdapter.notifyDataSetChanged();
+                    selectedPosition= -1;
+                }
+            }
+        });
+
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                selectedPosition=position;
+                inputLayout.setVisibility((View.GONE));
+            }
+        });
+    }
+
+}
